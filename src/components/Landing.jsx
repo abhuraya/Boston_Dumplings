f18b9b5be99d34046cb3d0892b0c3fc3eb016c51
abhuraya <abhuraya@gmail.com>
2024-12-17 09:06:52 -0500

    Landing update

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,7 +1,13 @@
+import { useState } from 'react'
 export default function Landing(){
+
+    const [count, setCount] = useState(0);
     return(
         <div>
             <h6>Namaste MOM!!!</h6>
+            <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
         </div>
     )
 }
