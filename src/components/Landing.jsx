46555496a812b4670f6a73c9380b88c568873b48
abhuraya <abhuraya@gmail.com>
2024-12-18 11:03:13 -0500

    form update

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -8,6 +8,23 @@ export default function Landing(){
             <button onClick={() => setCount((count) => count + 1)}>
                 count is {count}
             </button>
+            <form action="" method='post'>
+                <div class='nameInput'>
+                    <label htmlFor="name">Enter your name: </label>
+                    <input type="text" name='name' id='name' required/>
+                </div>
+                <div class='emailInput'>
+                    <label htmlFor="email">Enter your email: </label>
+                    <input type="email" name="email" id='email' required />
+                </div>
+                <div class='comment'>
+                    <label htmlFor="comment">Comment</label>
+                    <input type="text" name='comment' id='comment'/> 
+                </div>
+                <div>
+                    <button>Submit</button>
+                </div>
+            </form>
         </div>
     )
 }
